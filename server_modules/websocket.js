f01e25a8c187ee0e9abd3329fa324f359981a6f8
SlowSoulWen <664381601@qq.com>
2018-04-05 12:00:11 +0800

    完善后台socket逻辑

--- server_modules/websocket.js
+++ server_modules/websocket.js
@@ -1,4 +1,10 @@
-function websocket (io) {
+const User = require('./collections/user')
+const Room = require('./collections/room')
+const Game = require('./collections/game')
+
+async function websocket (io) {
+  const roomCollection = await Room()
+
   // namespaces
   // 游戏大厅
   const HOME = io.of('/home')
@@ -17,28 +23,46 @@ function websocket (io) {
 
   // 初始化房间namespace
   ROOMS.on('connection', (socket) => {
+    let query = socket.handshake.query
     // 当有请求连接时，获取参数roomId并连接到具体的房间
-    let _roomId = socket.handshake.query.roomId
-    ROOMS.join(_roomId)
+    let _roomId = query.roomId
+    let nickName = query.nickName
+    let userId = query.userId
+
+    socket.join(_roomId, () => {
+      // 当有新玩家进入游戏时，广播通知
+      socket.broadcast.to(_roomId).emit('join', { nickName, userId })
+    })
 
     // 玩家在房间发言
     socket.on('message', (data) => {
-      ROOMS.emit('message', data)
+      ROOMS.to(_roomId).emit('message', {
+        message: data.message,
+        userId: userId
+      })
     })
-    // 有玩家加入房间
-    socket.on('join', (data) => {
-      ROOMS.emit('join', data)
-    })
+
     // 有玩家离开房间
-    socket.on('disconnect', (data) => {
+    socket.on('disconnect', async (data) => {
+      //  更新数据库
+      let room = await roomCollection.$findOneRoom({id: _roomId})
+      let player = room.player
+      let index = player.findIndex((player) => {
+        return player.id === userId
+      })
+      player.splice(index, 1)
+      await roomCollection.$updateOneRoom({ id: _roomId }, {
+        player
+      })
+      socket.broadcast.to(_roomId).emit('leave', {
+        userId: userId
+      })
       ROOMS.clients((error, clients) => {
         if (error) console.log('clients Error:', error)
         if (!clients.length) {
           // TODO：如果房间没人了，更改房间状态
         }
       })
-      // 广播有玩家离开房间
-      ROOMS.emit('leave', data)
     })
   })
 
