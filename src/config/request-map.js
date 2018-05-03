ce376c769ce8eb5fee0fc2b9bcb8d294729504f5
SlowSoulWen <664381601@qq.com>
2018-05-03 12:15:49 +0800

    更新请求接口

--- src/config/request-map.js
+++ src/config/request-map.js
@@ -1,33 +1,213 @@
 import $http from '@src/utility/fetch'
 // import querystring from 'querystring'
-import { ENVIRONMENT } from '@src/config/setting'
+import { ENVIRONMENT, PAGE_SIZE_DEFAULT } from '@src/config/setting'
+
+let accounts = [
+  {
+    account: 'wenguang',
+    password: '123'
+  },
+  {
+    account: 'wenguang1',
+    password: '123'
+  },
+  {
+    account: 'wenguang2',
+    password: '123'
+  },
+  {
+    account: 'wenguang3',
+    password: '123'
+  }
+]
 
 /* eslint-disable */
+
+// 登录注册模块
+
 let devLogin = async () => {
+  let random = Math.floor(Math.random() * 4)
+  let account = accounts[random]
   return await $http({
     method: 'post',
     url: {
       path: 'user-signin'
     },
     data: {
-      account: 'wenguang',
-      password: '123456789'
+      account: account.account,
+      password: account.password
     }
   }).then((res) => {
-    console.log('signin-res', res)
     return res.data
+  })
+}
+
+let login = async ({account, password}) => {
+  return await $http({
+    method: 'post',
+    url: {
+      path: 'user-signin'
+    },
+    data: {
+      password,
+      account
+    }
+  })
+}
+
+let checkLogin = async () => {
+  return await $http({
+    method: 'post',
+    url: {
+      path: 'check-login'
+    }
   })
 }
 
 let getUserInfo = async () => {
   if (ENVIRONMENT === 'DEV') {
     // 测试环境通过模拟账号登录
-    await devLogin()
+    let res = await checkLogin()
+    return res.data
   } else if (ENVIRONMENT === 'PRO') {
-    // TODO 正式环境通过微信授权登录
+    // TODO
   }
 }
 
+let register = async ({account, nickname, password, avator}) => {
+  return await $http({
+    method: 'post',
+    url: {
+      path: 'register'
+    },
+    data: {
+      account,
+      nickname,
+      password,
+      avator
+    }
+  })
+}
+
 export const userModel = {
-  getUserInfo
+  getUserInfo,
+  register,
+  login
+}
+
+// 房间模块
+
+let createRoom = async (room) => {
+  return await $http({
+    method: 'post',
+    url: {
+      path: 'create-room'
+    },
+    data: {
+      roomName: room.roomName,
+      playerMaxNum: room.playerMaxNum,
+      roomPwd: room.roomPwd
+    }
+  }).then((res) => {
+    return res.data
+  })
+}
+
+let getRoomDeatil = async (id) => {
+  return await $http({
+    method: 'get',
+    url: {
+      path: 'get-room-detail'
+    },
+    params: {
+      roomId: id
+    }
+  })
+}
+
+let getRoomsList = async ({ pageIndex, pageSize = PAGE_SIZE_DEFAULT }) => {
+  return await $http({
+    method: 'get',
+    url: {
+      path: 'get-rooms-list'
+    },
+    params: {
+      pageIndex,
+      pageSize
+    }
+  })
+}
+
+let joinRoom = async (roomId) => {
+  return await $http({
+    method: 'post',
+    url: {
+      path: 'join-room'
+    },
+    data: {
+      roomId
+    }
+  })
+}
+
+let searchRoom = async (roomName) => {
+  return await $http({
+    method: 'get',
+    url: {
+      path: 'search-room'
+    },
+    params: {
+      roomName
+    }
+  })
+}
+
+export const roomModel = {
+  createRoom,
+  getRoomDeatil,
+  getRoomsList,
+  joinRoom,
+  searchRoom
+}
+
+// 游戏模块
+let createGame = async (roomId) => {
+  return await $http({
+    method: 'post',
+    url: {
+      path: 'create-game'
+    },
+    data: {
+      roomId
+    }
+  })
+}
+
+let getGameDetail = async ({gameId, userId}) => {
+  return await $http({
+    method: 'get',
+    url: {
+      path: 'get-game-detail'
+    },
+    params: {
+      gameId,
+      userId
+    }
+  })
+}
+
+let updateRecord = async (record) => {
+  return await $http({
+    method: 'post',
+    url: {
+      path: 'update-record'
+    },
+    data: record
+  })
+}
+
+export const gameModel = {
+  createGame,
+  getGameDetail,
+  updateRecord
 }
--- src/config/request-url.js
+++ src/config/request-url.js
@@ -6,6 +6,20 @@ const REQUEST_URL = {}
 // 登录相关
 REQUEST_URL['user-signin'] = '/user/signin'
 REQUEST_URL['user-logout'] = '/user/signout'
+REQUEST_URL['check-login'] = '/checkLogin'
+REQUEST_URL['register'] = '/register'
+
+// 房间相关
+REQUEST_URL['create-room'] = '/createRoom'
+REQUEST_URL['get-room-detail'] = '/getRoomDetail'
+REQUEST_URL['get-rooms-list'] = '/getRoomsList'
+REQUEST_URL['join-room'] = '/joinRoom'
+REQUEST_URL['search-room'] = '/searchRoom'
+
+// 游戏相关
+REQUEST_URL['create-game'] = '/createGame'
+REQUEST_URL['get-game-detail'] = '/getGameDetail'
+REQUEST_URL['update-record'] = '/updateRecord'
 
 let requestUrl = REQUEST_URL
 for (let key in REQUEST_URL) {
